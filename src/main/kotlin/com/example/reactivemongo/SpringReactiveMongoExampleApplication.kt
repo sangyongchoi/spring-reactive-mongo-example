3f0b16f77b92c957103bddf7925615fef61d1f13
sangyongchoi <csy7792@naver.com>
2022-03-12 16:14:43 +0900

    init project

---
+++ src/main/kotlin/com/example/reactivemongo/SpringReactiveMongoExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.reactivemongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringReactiveMongoExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringReactiveMongoExampleApplication>(*args)
+}
