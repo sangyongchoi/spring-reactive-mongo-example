6390b9588423715b015e4d989181801dec58df00
sangyongchoi <csy7792@naver.com>
2022-03-13 18:56:30 +0900

    update README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # spring-reactive-mongo-example
 
 spring-boot-starter-data-mongodb-reactive 와 querydsl 을 함께 사용하는 예제 프로젝트입니다.
+
+### ReactiveQuerydslPredicateExecutor
+querydsl 과 함께 사용할 수 있는 Interface
+
+인자로 Predicate 를 넘길 수 있다.
+````kotlin
+suspend fun get(title: String): List<Demo> {
+    return demoRepository.findAll(demo.title.eq(title)).collectList().awaitSingle() ?: emptyList()
+}
+````
--- src/main/kotlin/com/example/reactivemongo/service/DemoService.kt
+++ src/main/kotlin/com/example/reactivemongo/service/DemoService.kt
@@ -1,7 +1,7 @@
 package com.example.reactivemongo.service
 
 import com.example.reactivemongo.document.Demo
-import com.example.reactivemongo.document.QDemo
+import com.example.reactivemongo.document.QDemo.demo
 import com.example.reactivemongo.repository.DemoRepository
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Service
@@ -11,12 +11,8 @@ class DemoService(
     private val demoRepository: DemoRepository,
 ) {
 
-    private val demo = QDemo.demo
-
-    suspend fun get(title: String): List<Demo>? {
-        val findAll = demoRepository.findAll(demo.title.eq(title))
-
-        return findAll.collectList().awaitSingle()
+    suspend fun get(title: String): List<Demo> {
+        return demoRepository.findAll(demo.title.eq(title)).collectList().awaitSingle() ?: emptyList()
     }
 
     suspend fun create(title: String) {
